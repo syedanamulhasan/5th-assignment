b74632bd9319d8cc3d67e9fc0c0ea7408ac8e5aa
syedanamulhasan <syedanamulhasan@gmail.com>
2024-12-08 18:05:28 +0600

    page toggling added

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,6 @@
 document.getElementById('show-donation-page').addEventListener('click',function(event){
     showDonationPage('donation-page')
 })
+document.getElementById('show-donation-history').addEventListener('click',function(event){
+    showDonationPage('history-page')
+})
--- js/function.js
+++ js/function.js
@@ -8,3 +8,9 @@ function floodDonationTotalAmount(id){
     const floodAmount = parseFloat(value);
     return floodAmount;
 }
+function showDonationPage(id){
+    document.getElementById('donation-page').classList.add('hidden');
+    document.getElementById('history-page').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
