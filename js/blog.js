f3ffdc98a545ab7fb0aeef12f5724d68243fd3ed
syedanamulhasan <syedanamulhasan@gmail.com>
2024-12-09 19:25:29 +0600

    navbar sticky done

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,27 @@
 document.getElementById('show-donation-page').addEventListener('click',function(event){
     showDonationPage('donation-page')
 })
-document.getElementById('show-donation-history').addEventListener('click',function(event){
-    showDonationPage('history-page')
+// document.getElementById('show-donation-history').addEventListener('click',function(event){
+//     showDonationPage('history-page');
+// })
+const historyPage = document.getElementById('show-donation-history');
+const donationPage = document.getElementById('show-donation-page');
+historyPage.addEventListener('click',function(){
+    showDonationPage('history-page');
+    historyPage.classList.add(
+        "bg-[#a3e635]"
+    )
+    donationPage.classList.remove(
+        "bg-[#a3e635]"
+    )
+})
+
+donationPage.addEventListener('click',function(){
+    showDonationPage('donation-page');
+    donationPage.classList.add(
+        "bg-[#a3e635]"
+    )
+    historyPage.classList.remove(
+        "bg-[#a3e635]"
+    )
 })
--- js/script.js
+++ js/script.js
@@ -2,22 +2,35 @@ document.getElementById('noakhali-flood-donation').addEventListener('click',func
     event.preventDefault();
     const amount = floodDonationAmount('flood-donation-amount');
     const totalAmount = floodDonationTotalAmount('total-flood-donation');
+    const reducedAmount = floodDonationTotalAmount('reduced-amount');
+    const newReducedAmount = reducedAmount - amount;
     
-    if(isNaN(amount)){
+    if(amount <= 0 || newReducedAmount <=0 || isNaN(amount)){
         alert('failed to donate');
-        return;
-    }
-    if(amount < 0){
-        alert('failed to donate');
-        return;
+        
+        return modal;
+        
     }
     else{
         document.getElementById('total-flood-donation').innerText =  amount + totalAmount +" " + "BDT";
+        document.getElementById('reduced-amount').innerText = newReducedAmount +" " + "BDT";
+        // const modal = document.getElementById('my_modal_1');
+        // modal.classList.remove("hidden")
     }
 
-    const reducedAmount = floodDonationTotalAmount('reduced-amount');
-    const newReducedAmount = reducedAmount - amount +" " + "BDT";
-    document.getElementById('reduced-amount').innerText = newReducedAmount;
+    const text = document.getElementById('flood-text').innerText;
+    const div= document.createElement('div');
+    div.classList.add (
+        "border-2",
+        "rounded-lg",
+        "p-2"
+    );
+    div.innerHTML = `
+    <p class="text-xl font-semibold">${amount} is donated ${text}</p>
+    <p>${new Date()}</p>
+    `
+    document.getElementById('history-page').appendChild(div)
+    
 })
 
 document.getElementById('feni-flood-donation').addEventListener('click',function(event){
@@ -38,6 +51,15 @@ document.getElementById('feni-flood-donation').addEventListener('click',function
     const reducedAmount = floodDonationTotalAmount('reduced-amount');
     const newReducedAmount = reducedAmount - feniAmount + " " + "BDT";
     document.getElementById('reduced-amount').innerText = newReducedAmount;
+
+    const text = document.getElementById('flood-text').innerText;
+    const div= document.createElement('div');
+    div.innerHTML = `
+    <p class="text-xl font-semibold">${amount} is donated ${text}</p>
+    <p>Date : <time datetime="2008-02-14 20:00">Bangladesh standard time</time></p>
+    `
+    
+    document.getElementById('history-page').appendChild(div)
 })
 
 document.getElementById('quota-protest-donation').addEventListener('click',function(event){
